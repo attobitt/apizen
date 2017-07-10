d6356f02968e175aa08b8f359ba0cd16892dc9a5
Atto Bitt <attobitt@gmail.com>
2017-07-10 17:30:05 -0400

    .

--- js/agency.js
+++ js/agency.js
@@ -15,7 +15,7 @@
   // Highlight the top nav as scrolling occurs
   $('body').scrollspy({
     target: '.navbar-fixed-top',
-    offset: 40
+    offset: 50
   });
 
   // Closes the Responsive Menu on Menu Item Click
